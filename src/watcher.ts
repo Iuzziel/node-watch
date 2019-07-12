34ffc677cb71cbfaa2107f02ab635fe6c01c0eb4
iuzziel <iuzziel@hotmail.fr>
2019-07-12 15:31:44 +0200

    v0.2.0

--- src/watcher.ts
+++ src/watcher.ts
@@ -3,34 +3,33 @@ import fs from 'fs';
 import path from 'path';
 
 var child: child_process.ChildProcess;
+var watcher: fs.FSWatcher;
 
 export default function watch(): void {
-	console.log('watch()');
 	child = child_process.spawn(
 		process.argv0,
 		process.argv.filter(v => {
-			if (v.search(/^\w*watch\.js$/) > 0)
+			if (v.search(/(^\S*@iuzziel[\\\/]+node-watch-lib[\\\/]+lib[\\\/]+index[\.js]*$|^\S*watch[\.js]*$)/) >= 0 || process.argv0 === v)
 				return false;
-			else if (process.argv0 === v)
-				return false;
-			else return true;
+			else
+				return true;
 		}),
 		{ detached: false, stdio: "inherit" }
 	);
 
 	child
-		.on('close', (code, signal) => { })
-		.on('error', (err) => { console.log('error', err); child.kill(); })
-		.on('exit', (code, signal) => { console.log('exit', code, signal); watch(); });
+		.on('error', (err) => { child.kill(); throw err; })
+		.on('exit', (code, signal) => { console.log(`\x1b[34m< Watcher ask for restart >\x1b[0m`); watch(); });
 
-	fs.watch(path.resolve(process.cwd()), { recursive: true }, (evt, fname) => {
-		let t = 1000;
-		if (fname.search(/\.js$/) > 0) t = 1;
-		else if (fname.search(/\.ts$/) > 0) return;
-		else return;
-		setTimeout(() => {
-			console.log(`File ${fname} changed.`);
+	watcher = fs.watch(path.resolve(process.cwd()), { persistent: false, recursive: true }, (evt, fname) => {
+		if (fname.search(/\.js$/) > 0) {
+			watcher.close();
 			child.kill();
-		}, t)
+		}
+		else if (fname.search(/\.ts$/) > 0) {
+			return;
+		} else {
+			return
+		};
 	});
 }
